c8eda8a0a2ce581ddf5cfdf921fc695f421d7ef4
Karan Chaurasiya <107132330+iamkc07@users.noreply.github.com>
2022-07-11 21:00:49 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,44 @@
+<html>
+<head>
+<title> Form1 </title>
+</head>
+<body>
+ <h1 align="center"> Data Entry Form </h1><br><br>
+<center>
+
+<div>
+Enter Name: 
+<input type="text"><br><br>
+Enter Password :
+<input type="password"><br><br>
+Select Gender :
+<input type="radio" name="b">Male
+<input type="radio" name="b">Female<br><br>
+Enter Address :
+<textarea coll="50" rows="5">
+</textarea><br><br>
+</div>
+
+<div>
+Select Subject :<br><br>
+<input type="checkbox">Human Resoures Management<br>
+<input type="checkbox">Web Technology<br>
+<input type="checkbox">Data Struture<br>
+<input type="checkbox">Computer Network<br>
+<input type="checkbox">Maths<br><br>
+</div>
+
+<div>
+Select City :
+<select>
+<option value="Select" selected>Select
+<option value="Kohlapur">Kohlapur
+<option value="Mumbai">Mumbai
+<option value="Pune">Pune
+<option value="Ichalkaranji">Ichalkaranji
+</select><br><br>
+</div>
+
+<input type="Submit">   <input type="Reset"></center>
+</body>
+</html>
